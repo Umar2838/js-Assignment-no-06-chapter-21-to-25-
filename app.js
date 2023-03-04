5f6668e12932a6ea31c5c6536aa1dbacc6580393
Umar Aqeel <112412509+Umar2838@users.noreply.github.com>
2023-03-05 01:43:15 +0500

    added a comment

---
+++ app.js
@@ -0,0 +1,74 @@
+//<**********************************Chapter no 21*******************************>
+
+//Question no 01
+
+// var firstName = prompt("Enter First Name")
+// var lastName = prompt("Enter Last Name")
+// var fullName = firstName + lastName
+// alert("Hi, "+ fullName +". How are you" )
+
+//Question no 02
+
+// var mobileName = prompt("Enter your favourite Mobile Name")
+// document.write("My favourite Phone Name is: " + mobileName + "</br>" )
+// document.write("Lenght of the string is: "+mobileName.length)
+
+//Question no 03
+
+// var string = "Pakistani"
+// document.write("String: "+string)
+// document.write("</br>Index of 'n' is: " + string.indexOf("n"))
+
+//Question no 04
+
+// var string = "Hello world"
+// document.write("String: "+string)
+// document.write("</br>Index of 'n' is: " + string.lastIndexOf("l"))
+
+//Question no 05
+
+// var str = "Pakistani"
+// document.write("String: "+str)
+// document.write("</br>Character at Index 3 is: " + str.charAt(3))
+
+// Question no 06
+
+// var firstName = prompt("Enter First Name")
+// var lastName = prompt("Enter Last Name")
+// alert("Hi, "+ firstName.concat(lastName) +". How are you" )
+
+//Question no 07
+
+// var word = "Hyderabad"
+// document.write("City: "+word)
+// document.write("</br>After Replacement: " + word.replace("Hyder","Islam"))
+
+
+//Question no 08
+
+// var message = "Ali and Sam are best friend.They play cricket and football together."
+// document.write("Message: " + message)
+// document.write("</br>After Replacement: " + message.replaceAll("and","&"))
+
+
+//Question no 09
+
+// var numstr = "12"
+// document.write("Value: " + numstr)
+// document.write("</br>Type: " + typeof(numstr))
+// document.write("Value: " + +numstr)
+// document.write("</br>Type: " + typeof(numstr))
+
+//Question no 10
+
+// var input = prompt("Enter any word")
+// var result = input.toUpperCase()
+// alert(result)
+
+
+//Question no 11
+
+var input = "javascript"
+
+var result = input.toUpperCase()
+alert(result)
